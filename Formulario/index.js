3010eecab1fa1737a5cd87f8a4f62605e5f2cf1e
Chuni14 <102385789+Chuni14@users.noreply.github.com>
2022-10-04 11:35:24 -0600

    Formulario

--- Formulario/index.js
+++ Formulario/index.js
@@ -1,4 +1,34 @@
-document.getElementById('btnSend').addEventListener('click', () => {
-    let usrname = document.getElementById('inputUsrname').value;
-    alert(usrname);
+document.getElementById('btn').addEventListener('click', () =>{
+    let item = document.getElementById('btn');
+    item.style.background = "blue";
+    item.style.color = "white";
+    item.style.fontFamily = "Times New Roman";
+    item.style.fontSize = "35px";
+    item.style.borderRadius = "20px";
 });
+
+document.getElementById('btn').addEventListener('click', () =>{
+    let usr = document.getElementById('inputUsrname').value;
+    let pswd = document.getElementById('inputPassword').value;
+    
+    if(usr === "Chuni" && pswd === "López"){
+        alert('Conexión establecida');
+        sessionStorage.setItem("login", "true");
+    }
+    else{
+        alert('Usuario/Contraseña Incorrecta');
+        sessionStorage.setItem("login", "false");
+    }
+    let session = sessionStorage.getItem('login');
+    console.log(session);
+});
+
+setInterval(()=> {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let item = document.getElementById("body")
+    let bck = "rgb("+ red + ", " + green + ", " + blue +")";
+    item.style.background = bck;
+    
+}, 300);
